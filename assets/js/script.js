714adc806e1a546797af5ffdb874d5b58263aeda
gustavinhoapm <gustavo.agostinho2021@hotmail.com>
2023-11-13 18:40:43 -0300

    att-semi-finalizado_13112023

--- assets/js/script.js
+++ assets/js/script.js
@@ -90,3 +90,20 @@ document.addEventListener("DOMContentLoaded", function () {
       img.style.filter = 'grayscale(100%)';
     });
   });
+
+
+
+  // SCRIPT PARA O ACORDION FUNCIONAR
+  document.addEventListener('DOMContentLoaded', function () {
+    const accordionItems = document.querySelectorAll('.accordion-item');
+
+    accordionItems.forEach(function (item) {
+        const header = item.querySelector('.accordion-header');
+
+        header.addEventListener('click', function () {
+            item.classList.toggle('active');
+            const content = item.querySelector('.accordion-content');
+            content.style.display = item.classList.contains('active') ? 'block' : 'none';
+        });
+    });
+});
